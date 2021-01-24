bb05d9cebd4f658288627994975aa715a045076b
han6054 <424681370@qq.com>
2021-01-24 17:37:19 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h1>这是页面内容</h1>
+    <visual-editor/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {VisualEditor} from '@/packages/visual-editor'
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    VisualEditor,
   }
-}
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   html, body {
+        margin: 0;
+        padding: 0;
+    }
 </style>
